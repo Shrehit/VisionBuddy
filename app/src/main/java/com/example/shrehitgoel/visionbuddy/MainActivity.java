dd8e4012cbe46fbe987b0619974d4c67b2725069
shrehit <sgoel22@wisc.edu>
2019-04-25 04:02:59 -0500

    Added ability to get image from url

--- app/src/main/java/com/example/shrehitgoel/visionbuddy/MainActivity.java
+++ app/src/main/java/com/example/shrehitgoel/visionbuddy/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.shrehitgoel.visionbuddy;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
@@ -19,11 +22,13 @@ import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,6 +36,8 @@ import android.widget.Toast;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -72,6 +79,13 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         readButton = findViewById(R.id.readButton);
         cameraButton = findViewById(R.id.camera_button);
         Button stopButton = findViewById(R.id.stop);
+
+        Button urlButton = findViewById(R.id.urlBut);
+
+        urlButton.setOnClickListener(v->
+        {
+            getUrl();
+        });
 
         savedImage = false;
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -155,6 +169,45 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         stopButton.setOnClickListener((v) -> {
             tts.stop();
         });
+    }
+
+    // TODO: add ability to save images that we get from url
+    void getUrl()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("URL:");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", (dialog, which) ->
+        {
+            String url = input.getText().toString();
+            Picasso.get().load(url).into(new Target() {
+                @Override
+                public void onBitmapLoaded(Bitmap bit, Picasso.LoadedFrom from) {
+                    bitmap = bit;
+                    imageView.setImageBitmap(bit);
+                }
+
+                @Override
+                public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+
+                }
+
+                @Override
+                public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                }
+            });
+        });
+        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
+
+        builder.show();
     }
 
     void createExternalStoragePublicPicture() {
